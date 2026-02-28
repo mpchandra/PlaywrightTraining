5a610e853cd39b5b70be766481d5dd7f8ddb683e
mpchandra <92070993+mpchandra@users.noreply.github.com>
2026-02-28 23:22:18 +0530

    Add files via upload

---
+++ Week1Day1/LetAssignment.js
@@ -0,0 +1,11 @@
+let userName = "PoornaChandra"
+let organization = "WellsFargo US Bank"
+let contactNumber = 9678454458
+let knowsAutomation = true
+let usesPlaywright
+
+console.log("Type " + typeof userName + " Value " + userName)
+console.log("Type " +typeof organization+ " Value " + organization)
+console.log("Type " +typeof contactNumber+ " Value " + contactNumber)
+console.log("Type " +typeof knowsAutomation+ " Value " + knowsAutomation)
+console.log("Type " +typeof usesPlaywright+ " Value " + usesPlaywright)
